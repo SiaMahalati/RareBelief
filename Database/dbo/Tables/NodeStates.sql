8ecd2a13eff6d03e5116acc285993feb0927b177
Sia Mahalati <siamahalati@gmail.com>
2019-06-30 16:14:31 -0400

    Commit All

--- Database/dbo/Tables/NodeStates.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[NodeStates]
-(
-	[Id]	INT IDENTITY (1,1) NOT NULL,
-	[StateId]		INT			   NULL,
-	[NodeId]		INT			   NULL,
-	CONSTRAINT [PK_dbo.NodeStates] PRIMARY KEY CLUSTERED ([Id] ASC),
-	CONSTRAINT [FK_dbo.NodeStates_dbo.States_State] FOREIGN KEY ([StateId]) REFERENCES [dbo].[States] ([Id]),
-	CONSTRAINT [FK_dbo.NodeStates_dbo.Nodes_NodeId] FOREIGN KEY ([NodeId]) REFERENCES [dbo].[Nodes] ([Id])
-)
--- Database/dbo/Tables/Nodes.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Nodes]
-(
-	[Id]	INT IDENTITY (1,1) NOT NULL,
-	[Name]	NVARCHAR(255) NULL,
-	[DateCreated]   DATETIME       NOT NULL,
-	[ParentNodeId]		INT			   NULL,
-	CONSTRAINT [PK_dbo.Node] PRIMARY KEY CLUSTERED ([Id] ASC),
-	CONSTRAINT [FK_dbo.Node_dbo.Node_ParentNodeId] FOREIGN KEY ([ParentNodeId]) REFERENCES [dbo].[Nodes] ([Id])
-)
